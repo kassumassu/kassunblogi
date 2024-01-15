c2e3f5b467184a6807ccfb9b2a6fc609614fdb0b
kassumassu <142390041+kassumassu@users.noreply.github.com>
2024-01-15 20:54:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Tervetuloa blogiini!"); 
+function x() {
+    alert("tykkään koirista!"); 
+} 
+function s() {
+    alert("Paina näppäimistöstäsi mitä tahansa näppäintä!"); 
+} 
+const f = document.body.addEventListener("keypress", (ev) => {
+    alert("minulla on koira nimeltä leo hän on tiibetin spanieli"); 
+}); 
